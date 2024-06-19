72fab77a91a191a8447a4d33f71e71cb22f66d3c
Kavya Chauhan <kavyachauhan910@gmail.com>
2024-06-19 19:48:30 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-import {InputForm} from "./Input";
+
+function NameForm(props) {
+    const [formData, setFormData] = useState({ name: "", time: "" });
+
+    function changeHandle(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    function submitHandler(e) {
+        e.preventDefault();
+        const currentTime = new Date().toLocaleTimeString();
+        props.onAdd({ ...formData, time: currentTime });
+        setFormData({ name: "", time: "" });
+    }
+
+    return (
+        <>
+            <h1>Employee Attendance</h1>
+            <form onSubmit={submitHandler}>
+                <input type="text" name="name" placeholder="Name" value={formData.name} onChange={changeHandle} />
+                <button>Add The Attendance</button>
+            </form>
+        </>
+    );
+}
+
+function ListofPeople(props) {
+    return (
+        <div className="list-container">
+            <ul>
+                {props.allWorkers.map((worker, index) => (
+                    <li key={index}>
+                        <span>{worker.name} checked in at {worker.time}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <InputForm/>
-  );
+    const [allWorkers, updateAllWorkers] = useState([]);
+
+    function addName(newWorker) {
+        updateAllWorkers(prevWorkers => [...prevWorkers, newWorker]);
+    }
+
+    return (
+        <div className="App">
+            <NameForm onAdd={addName} />
+            <ListofPeople allWorkers={allWorkers} />
+        </div>
+    );
 }
 
 export default App;
